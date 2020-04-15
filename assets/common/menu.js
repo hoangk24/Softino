186b63217ed3c6be202a060462cf79db7bf8013c
truongkawaii <60839803+truongkawaii@users.noreply.github.com>
2020-04-15 14:39:51 +0700

    upbtap

--- assets/common/menu.js
+++ assets/common/menu.js
@@ -1,11 +1,14 @@
 const menuMobile = {
     init: function() {
         this.clickShowmenu('.humberger', '.menu__list', 'showmenu');
-        this.clickShowmenu('#item2', '#item2 .drop__menu', 'showMenu');
-        this.clickShowmenu('#item3', '#item3 .drop__menu', 'showMenu');
-        this.clickShowmenu('#item4', '#item4 .drop__menu', 'showMenu');
-        this.clickShowmenu('#item5', '#item5 .drop__menu', 'showMenu');
-        this.clickShowmenu('#item6', '#item6 .drop__menu', 'showMenu');
+        this.clickShowmenu('#item2 .list--item-black', '#item2 .drop__menu', 'showMenu');
+        this.clickShowmenu('#item3 .list--item-black', '#item3 .drop__menu', 'showMenu');
+        this.clickShowmenu('#item4 .list--item-black', '#item4 .drop__menu', 'showMenu');
+        this.clickShowmenu('#item5 .list--item-black', '#item5 .drop__menu', 'showMenu');
+        this.clickShowmenu('#item6 .list--item-black', '#item6 .drop__menu', 'showMenu');
+        this.clickShowmenu('#item7 h5', '#item7 .drop__menu2', 'showmenu2');
+        this.clickShowmenu('#item8 h5', '#item8 .drop__menu2', 'showmenu2');
+
     },
     clickShowmenu: function(button, menu, togeMenu) {
         let btn = document.querySelector(button);
